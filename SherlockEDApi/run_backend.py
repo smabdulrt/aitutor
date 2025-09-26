c6b06c5d32cd61dedb48817f2db30dad51bd6c18
ubterko <ebonkoisrael@gmail.com>
2025-09-27 00:47:34 +0100

    Fixed a11y linting issues

--- SherlockEDApi/run_backend.py
+++ SherlockEDApi/run_backend.py
@@ -1,9 +1,9 @@
-from main import app 
+from app.main import app 
 import uvicorn 
 
 if __name__ == "__main__":
     uvicorn.run(
-        app, 
+        "app.main:app", 
         host="0.0.0.0", 
         port=8001,
         reload=True
